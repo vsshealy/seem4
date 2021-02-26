aa45475fbf4464abc2676679e995f2005518d70c
Scott Shealy <vsshealy@gmail.com>
2021-02-26 10:21:22 -0500

    Added footer

--- footer.php
+++ footer.php
@@ -7,3 +7,13 @@
      * @copyright 02/2021
     **/
 ?>
+
+<footer id="footer-primary">
+
+    <!-- #FOOTER-MAIN -->
+    <?php include(get_template_directory()."/php/footer/footer-main.php"); ?>
+
+    <!-- #FOOTER-COPYRIGHT -->
+    <?php include(get_template_directory()."/php/footer/footer-copyright.php"); ?>
+
+</footer>
