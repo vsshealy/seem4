0088fe2e029b002fce198cdbd40c926befc583f5
Scott Shealy <vsshealy@gmail.com>
2021-02-26 10:42:46 -0500

    Added base page functions

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/**
+ * js/scripts.js
+ * @package seem
+ * @author Scott Shealy
+ * @version 1.0.0
+ * @copyright 02/2021
+**/
---
+++ php/theme/scripts/body/body-scripts.php
@@ -0,0 +1,11 @@
+<?php 
+    /**
+     * php/theme/scripts/body/body-scripts.php
+     * @package SEEM
+     * @author Scott Shealy
+     * @version 1.0.0
+     * @copyright 02/2021
+    **/
+?>
+
+<?php wp_body_open(); ?>
